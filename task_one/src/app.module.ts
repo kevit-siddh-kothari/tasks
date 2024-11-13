72e92100cfe5699dc60a92a308a4be194b2e4624
siddh-kothari <siddh.kothari@kevit.io>
2024-11-14 00:03:01 +0530

    feat: Profile component with CRUD operations added

--- task_one/src/app.module.ts
+++ task_one/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './components/users/users.module';
+import { ProfileModule } from './components/profile/profile.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
